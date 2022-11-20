ee213deb05516e00bde6502f1ac33c2674d59de8
james fryer <jamesfryer1998@outlook.com>
2022-11-20 22:16:35 +0000

    All files addition

---
+++ main/test.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '/Users/james/Projects/arbitrage')
+from useful_functions import load_json
+
+print(load_json('/Users/james/Projects/arbitrage/crypto_download/api.json')['API_KEY'])
+
---
+++ useful_functions.py
@@ -0,0 +1,23 @@
+import json
+import datetime
+import os
+import re
+import pandas as pd
+
+def load_json(path):
+    with open(path) as f:
+        data = json.load(f)
+        return data
+
+def remove_date_files(file_path):
+    time = datetime.datetime.now()
+    files = os.listdir(file_path)
+    for file in files:
+        search = re.search(f'{time.date()}', file)
+        if search == None:
+            os.remove(f'{file_path}/{file}')
+            print(f'{file} removed')
+
+def time_convert(unix):
+     return datetime.datetime.utcfromtimestamp(1668902400).strftime('%Y-%m-%d') 
+
