6c24143a702fd596432fd15fcc66cce3a94335fb
james fryer <jamesfryer1998@outlook.com>
2022-12-03 15:20:34 +0000

    fees alterations

--- arbitrage_system/exchange_arbitrage.py
+++ arbitrage_system/exchange_arbitrage.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import json
 from datetime import datetime
 sys.path.insert(0, '/Users/james/Projects/arbitrage')
 from useful_functions import load_json
@@ -12,7 +13,8 @@ class Arbitrage:
         self.exchanges = exchanges
         self.cache_path = cache_path
         # ADD IN FEES HERE, JUST A LIST OF FEES FROM EXCHANGES
-        self.fees = None 
+        ref_data = load_json('/Users/james/Projects/arbitrage/crypto_download/symbol_list.json')
+        self.fees = ref_data['exchange_fees']
         self.symbol = None
         self.arbitrage = None
         
@@ -48,22 +50,36 @@ class Arbitrage:
 
         selling = sorted_arbitrage[-1]
         buying = sorted_arbitrage[0]
-        print(f'Buy: {buying}')
-        print(f'Sell: {selling}')
+        if buying['time'] == selling['time']:
+            print(f'Buy: {buying}')
+            print(f'Sell: {selling}')
 
-        diff = selling['close'] - buying['close']
+            diff = selling['close'] - buying['close']
 
-        # Account for fees
-        # if diff - fees > 1:
-        #   buy/sell
+            for key, value in self.fees.items():
+                if selling['exchange'] == key:
+                    selling_fee = value
+                elif buying['exchange'] == key:
+                    buying_fee = value
+    
+            all_fees = selling_fee + buying_fee
+            print(f'diff: {diff}')
+            print(f'fees: {all_fees}')
 
-        if diff > 0.1:
-            print(diff)
+            if diff > all_fees:
+                if os.path.exists("/Users/james/Projects/arbitrage/arbitrage_system/arbitrage_opportunities.json") == False:
+                    ########################### HERERERERE
+                    with open("sample.json", "w") as outfile: 
+                        json.dump(dictionary, outfile) 
+
+                print(diff-all_fees)
+        else:
+            print(f'Arbitrage time stamps dont match: {buying["time"]} / {selling["time"]}')
 
     def arbitrage_opportunities(self):
         return
 
-arb = Arbitrage('BTC', 'USDT', '1', '/Users/james/Projects/arbitrage/crypto_download/cache')
+arb = Arbitrage('ETH', 'USD', '1', '/Users/james/Projects/arbitrage/crypto_download/cache')
 arb.compare_matching_symbols()
 arb.profitable_exchanges()
 
