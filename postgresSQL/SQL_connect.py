7ed2da1c850f769db3c81be42fbe1bbd3a7d4897
james fryer <jamesfryer1998@outlook.com>
2022-12-13 15:04:41 +0000

    SQL connection

--- postgresSQL/SQL_connect.py
+++ postgresSQL/SQL_connect.py
@@ -2,16 +2,26 @@ import os
 import sys
 import pandas as pd
 from datetime import datetime
+import psycopg2
+import psycopg2.extras as extras
 sys.path.insert(0, '/Users/james/Projects/arbitrage')
 from useful_functions import load_json
 tri_arb_path = '/Users/james/Projects/arbitrage/arbitrage_system/triangular_arbitrage.json'
 
 class PostgresSQL:
     def __init__(self, file_path):
+        self.connect_string = None
+        self.conn = psycopg2.connect(self.connect_string)
         self.time = datetime.now()
         self.file_path = file_path
         self.df = None
         self.convert_to_df()
+        self.connect_SQL_server()
+
+    def connect_SQL_server(self):
+        postgres_login = load_json('/Users/james/Projects/SQL/dashboard/crypto/postgres_login.json')
+        self.connect_string = f"host={postgres_login['host']} dbname=postgres user={postgres_login['user']} password={postgres_login['password']}"
+        self.conn = psycopg2.connect(self.connect_string)
 
     def convert_to_df(self):
         file = load_json(self.file_path)
@@ -37,6 +47,28 @@ class PostgresSQL:
         self.df = pd.DataFrame(list_dict)
         # print(self.df)
 
-    # def create_table(self):
+    def create_table(self):
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {self.ticker} (
+            id SERIAL PRIMARY KEY,
+            base CHAR(n),
+            base_price FLOAT(4),
+            inter CHAR(n),
+            inter_price FLOAT(4),
+            end CHAR(n),
+            end_price FLOAT(4),
+            final_investment FLOAT(4),
+            profit FLOAT(4),
+            date DATE
+        );
+        '''
+        with self.conn:
+            with self.conn.cursor() as cur:
+                try:
+                    cur.execute(query)
+                    self.conn.commit()
+                except psycopg2.errors.DuplicateTable as err:
+                    pass
+
 
 SQL = PostgresSQL(tri_arb_path)
